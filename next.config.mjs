2a5397e8a2df49a175f8a0c2dcc966ce402d4b09
Surya <ksaisurya304@gmail.com>
2024-10-01 18:08:17 -0400

    resolved Middleware error

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,20 @@
-// next.config.mjs
-export default {
-  async redirects() {
-    return [
-      {
-        source: "/admin",
-        destination: "/login",
-        permanent: false,
-      },
-    ];
-  },
-};
+// // next.config.mjs
+// export default {
+//   async redirects() {
+//     return [
+//       {
+//         source: "/admin",
+//         destination: "/login",
+//         permanent: false,
+//       },
+//     ];
+//   },
+// };
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: false,
+  swcMinify: true,
+}
+
+export default nextConfig;
--- pages/middleware.js
+++
@@ -1,7 +0,0 @@
-
-// middleware.js
-
-export function middleware(req, res, next) {
-  console.log(`${req.method} ${req.url}`);
-  next();
-}
